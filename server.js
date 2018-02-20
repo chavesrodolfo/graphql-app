7e21c1752348b8521059f99e7f1407b1acc2f8eb
Rodolfo <chavesrodolfo@gmail.com>
2018-02-19 22:18:36 -0300

    + babel-watch

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 import express from 'express';
+const server = express();
+
+server.get('/status', (req, res) => {
+    res.send({ 'status': 'ok' });
+});
+
+server.listen(4000, () => {
+    console.log('Listening http://localhost:4000');
+});
