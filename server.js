8f79cc1727f4c84dd03cf054ca196def0360cc9b
Rodolfo <chavesrodolfo@gmail.com>
2018-02-19 22:34:50 -0300

    graphiql endpoint and body-parser

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 import express from 'express';
+import { graphqlExpress, graphiqlExpress } from "apollo-server-express";
+import bodyParser from 'body-parser';
+
 const server = express();
+
+server.use('/graphiql', graphiqlExpress({
+    endpointURL:'graphql'
+}));
+
+server.use('/graphql', bodyParser.json(), graphqlExpress({
+
+}));
 
 server.get('/status', (req, res) => {
     res.send({ 'status': 'ok' });
